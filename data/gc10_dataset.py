f9b32546d4a39532356898d191b48e6d98a8c833
Kusumita-Patra <kusumita051006@gmail.com>
2026-07-07 06:53:50 +0000

    Added weighted random sampler

--- data/gc10_dataset.py
+++ data/gc10_dataset.py
@@ -1,5 +1,8 @@
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+from collections import Counter
+from torch.utils.data import WeightedRandomSampler
+import torch
 
 from config import (
     TRAIN_DIR,
@@ -56,14 +59,36 @@ def get_dataloaders():
         root=TEST_DIR,
         transform=val_transform,
     )
+    #---------------------------
+    # Compute Sample Weights
+    #---------------------------
+    targets = train_dataset.targets
+
+    class_counts = Counter(targets)
+
+    class_weights = {
+        cls: 1.0 / count
+        for cls, count in class_counts.items()
+    }
+
+    sample_weights = [
+        class_weights[label]
+        for label in targets
+    ]
+
+    sampler = WeightedRandomSampler(
+        weights=torch.DoubleTensor(sample_weights),
+        num_samples=len(sample_weights),
+        replacement=True
+    )
 
     train_loader = DataLoader(
-        train_dataset,
-        batch_size=BATCH_SIZE,
-        shuffle=True,
-        num_workers=NUM_WORKERS,
-        pin_memory=PIN_MEMORY,
-    )
+      train_dataset,
+      batch_size=BATCH_SIZE,
+      sampler=sampler,
+      num_workers=NUM_WORKERS,
+      pin_memory=True
+  )
 
     val_loader = DataLoader(
         val_dataset,
